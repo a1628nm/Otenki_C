4a6f93b63f9d0840e0a6f8aa9e9e4cb2fe276a78
mohumohu <b1621ry@aiit.ac.jp>
2016-08-06 17:48:00 +0900

    otenki_c.rbを編集

--- lib/Otenki_C.rb
+++ lib/Otenki_C.rb
@@ -1,5 +1,20 @@
 require "Otenki_C/version"
+#yahoo.rbの読み込み
+require "yahoo"
+#google geocodeの読み込み
+require "hogehoge"
+#表示用
+require "piyopiyo"
 
 module OtenkiC
-  # Your code goes here...
+  def otenki(add)
+      #郵便番号の取得
+      address　=　add
+      #郵便番号から緯度と経度を取得
+      
+      #緯度経度から天気の情報を取得
+      weatherResultHash = yahoo.weather(lng,lat)
+      #表示用
+  end  
+  module_function :otenki
 end
