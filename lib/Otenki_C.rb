161531fd6d0a2c86dba036de166483c81dcaa085
mohumohu <b1621ry@aiit.ac.jp>
2016-08-06 19:09:27 +0900

    require先のファイルの呼び出し場所のパスの変更

--- lib/Otenki_C.rb
+++ lib/Otenki_C.rb
@@ -1,20 +1,21 @@
 require "Otenki_C/version"
 #yahoo.rbの読み込み
-require "yahoo"
+require "Otenki_C/yahoo"
 #google geocodeの読み込み
-require "hogehoge"
+require "Otenki_C/hogehoge"
 #表示用
-require "piyopiyo"
+require "Otenki_C/piyopiyo"
 
 module OtenkiC
-  def otenki(add)
-      #郵便番号の取得
-      address　=　add
-      #郵便番号から緯度と経度を取得
-      
-      #緯度経度から天気の情報を取得
-      weatherResultHash = yahoo.weather(lng,lat)
-      #表示用
-  end  
-  module_function :otenki
+    def otenki(add)
+        #郵便番号の取得
+        address　=　add
+        #郵便番号から緯度と経度を取得
+        
+        #緯度経度から天気の情報を取得
+        weatherResultHash = yahoo.weather(lng,lat)
+        #表示用
+        
+    end
+    module_function :otenki
 end
